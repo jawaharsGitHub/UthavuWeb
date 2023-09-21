206fb14c1dfd58f13c1c41ed31c3094b297f5873
jawaharMPS <138604531+jawaharMPS@users.noreply.github.com>
2023-09-21 15:11:32 +0530

    initial commit

---
+++ Uthavu_Models/LearnBlazorModel/LearnBlazorModels.cs
@@ -0,0 +1,16 @@
+﻿namespace Uthavu_Models.LearnBlazorModel
+{
+	public class Product
+	{
+
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		public double Price { get; set; }
+		public bool IsActive { get; set; }
+
+		public IEnumerable<Size> Sizes { get; set; }
+
+
+	}
+}
---
+++ Uthavu_Models/LearnBlazorModel/Size.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uthavu_Models.LearnBlazorModel
+{
+	public class Size
+	{
+		public int SizeId { get; set; }
+		public string SizeName { get; set; }
+
+		public double SizePrice { get; set; }
+	}
+}
